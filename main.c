03e06e2e48efd874544c3e1647fc7f890366e39d
Mike Perry <mike@mikeperry.io>
2018-09-30 02:40:41 -0400

    refactored to graal_eval function

--- main.c
+++ main.c
@@ -68,21 +68,40 @@ long slurp(char const* path, char **buf, bool add_nul)
     return (long)fsz;
 }
 
-int main(int argc, char **argv) {
+int graal_eval(const char *language, const char *source) {
   graal_isolate_t *isolate = NULL;
   graal_isolatethread_t *thread = NULL;
-  
   if (graal_create_isolate(NULL, &isolate) != 0 || (thread = graal_current_thread(isolate)) == NULL) {
     fprintf(stderr, "initialization error\n");
     return 1;
   }
-  
   poly_context context = NULL;
   
   if (poly_create_context(thread, NULL, 0, &context) != poly_ok) {
     fprintf(stderr, "initialization error\n");
     return 1;
   }
+  poly_value result = NULL;
+  if (poly_context_eval(thread, context, language, "unicalc", source, &result) != poly_ok) {
+    fprintf(stderr, "eval error\n");
+    return 1;
+  }
+  char buffer[1024];
+  size_t length;
+
+  if (poly_value_to_string_utf8(thread, result, buffer, sizeof(buffer), &length) != poly_ok) {
+    fprintf(stderr, "to string error\n");
+    return 1;
+  }
+
+  buffer[length] = '\0';
+  printf("%s\n", buffer);
+
+  poly_destroy_handle(thread, result);
+  return 0;
+}
+
+int main(int argc, char **argv) {
   
   char* language = "js";
   
@@ -91,28 +110,13 @@ int main(int argc, char **argv) {
       language = &argv[n][1];
     } else {
       printf("language: %s\n", language);
-      poly_value result = NULL;
-      // TODO: write a function for reading the full file
       char *source;
       size_t size = slurp(argv[n], &source, false);
-
-      if (poly_context_eval(thread, context, language, "unicalc", source, &result) != poly_ok) {
-        fprintf(stderr, "eval error\n");
-        return 1;
-      }
+      int status = graal_eval(language, source);
       free(source);
-      char buffer[1024];
-      size_t length;
-      
-      if (poly_value_to_string_utf8(thread, result, buffer, sizeof(buffer), &length) != poly_ok) {
-        fprintf(stderr, "to string error\n");
+      if (status != 0) {
         return 1;
       }
-      
-      buffer[length] = '\0';
-      printf("%s\n", buffer);
-      
-      poly_destroy_handle(thread, result);
     }
   }
   
